ba9353e07c46928261e4c271c9dc9e94cacba926
javierlete <jglete@gmail.com>
2017-05-02 19:54:31 +0200

    Seguridad de ficheros y sistema de logout con persistencia de media hora incluyendo cierre de navegador

--- HolaMundo/WebContent/WEB-INF/vistas/principal.jsp
+++ HolaMundo/WebContent/WEB-INF/vistas/principal.jsp
@@ -10,7 +10,7 @@
 	<h1>Principal</h1>
 	<jsp:useBean id="usuario" scope="session" 
 		class="com.ipartek.ejemplos.javierlete.tipos.Usuario" />
-	<h2>Bienvenido ${usuario.nombre}</h2>
+	<h2>Bienvenido ${usuario.nombre} <a href="login?opcion=logout">Logout</a></h2>
 	<h3><%= new java.util.Date() %></h3>
 </body>
 </html>
--- HolaMundo/src/com/ipartek/ejemplos/javierlete/controladores/LoginServlet.java
+++ HolaMundo/src/com/ipartek/ejemplos/javierlete/controladores/LoginServlet.java
@@ -4,9 +4,11 @@ import java.io.IOException;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import com.ipartek.ejemplos.javierlete.dal.UsuariosDAL;
 import com.ipartek.ejemplos.javierlete.dal.UsuariosDALFijo;
@@ -16,6 +18,12 @@ import com.ipartek.ejemplos.javierlete.tipos.Usuario;
 public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
+	private static final String RUTA = "/WEB-INF/vistas/";
+	private static final String RUTA_PRINCIPAL = RUTA + "principal.jsp";
+	private static final String RUTA_LOGIN = RUTA + "login.jsp";
+
+	private static final int TIEMPO_INACTIVIDAD = 30 * 60;
+
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		doPost(request, response);
 	}
@@ -24,6 +32,8 @@ public class LoginServlet extends HttpServlet {
 		// Recoger datos de vistas
 		String nombre = request.getParameter("nombre");
 		String pass = request.getParameter("pass");
+
+		String opcion = request.getParameter("opcion");
 
 		// Crear modelos en base a los datos
 		Usuario usuario = new Usuario();
@@ -37,17 +47,45 @@ public class LoginServlet extends HttpServlet {
 		// contenido de un usuario "javi", "lete"
 		usuarioDAL.alta(new Usuario("javi", "lete"));
 
+		HttpSession session = request.getSession();
+		session.setMaxInactiveInterval(TIEMPO_INACTIVIDAD);
+
+		Cookie cookie = new Cookie("JSESSIONID", session.getId());
+		cookie.setMaxAge(TIEMPO_INACTIVIDAD);
+		response.addCookie(cookie);
+
+		// for (Cookie cookie : request.getCookies()) {
+		// if ("JSESSIONID".equals(cookie.getName())) {
+		// cookie.setMaxAge(TIEMPO_INACTIVIDAD);
+		// response.addCookie(cookie);
+		// }
+		// }
+
+		// ESTADOS
 		boolean esValido = usuarioDAL.validar(usuario);
 
+		boolean sinParametros = usuario.getNombre() == null;
+
+		boolean esUsuarioYaRegistrado = session.getAttribute("usuario") != null;
+
+		boolean quiereSalir = "logout".equals(opcion);
+
 		// Redirigir a una nueva vista
-		if (esValido) {
-			request.getSession().setAttribute("usuario", usuario);
+		if (quiereSalir) {
+			session.invalidate();
+			request.getRequestDispatcher(RUTA_LOGIN).forward(request, response);
+		} else if (esUsuarioYaRegistrado) {
+			request.getRequestDispatcher(RUTA_PRINCIPAL).forward(request, response);
+		} else if (sinParametros) {
+			request.getRequestDispatcher(RUTA_LOGIN).forward(request, response);
+		} else if (esValido) {
+			session.setAttribute("usuario", usuario);
 			// response.sendRedirect("principal.jsp");
-			request.getRequestDispatcher("/WEB-INF/principal.jsp").forward(request, response);
+			request.getRequestDispatcher(RUTA_PRINCIPAL).forward(request, response);
 		} else {
 			usuario.setErrores("El usuario y contraseña introducidos no son válidos");
 			request.setAttribute("usuario", usuario);
-			request.getRequestDispatcher("login.jsp").forward(request, response);
+			request.getRequestDispatcher(RUTA_LOGIN).forward(request, response);
 		}
 	}
 }
