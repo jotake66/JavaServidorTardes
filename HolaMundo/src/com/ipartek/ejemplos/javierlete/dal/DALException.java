0ab69b2371ef42a8e643b473c4333e1a71e6f4b1
javierlete <jglete@gmail.com>
2017-05-05 19:22:22 +0200

    Corrección pequeña

--- HolaMundo/src/com/ipartek/ejemplos/javierlete/dal/DALException.java
+++ HolaMundo/src/com/ipartek/ejemplos/javierlete/dal/DALException.java
@@ -2,9 +2,6 @@ package com.ipartek.ejemplos.javierlete.dal;
 
 public class DALException extends RuntimeException {
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = -7536930890973019777L;
 
 	public DALException() {
@@ -25,7 +22,6 @@ public class DALException extends RuntimeException {
 
 	public DALException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
 		super(message, cause, enableSuppression, writableStackTrace);
-		// TODO Auto-generated constructor stub
 	}
 
 }
